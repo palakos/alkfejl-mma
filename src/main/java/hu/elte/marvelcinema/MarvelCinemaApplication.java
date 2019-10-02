21950a1988c5229131cc40447e8f11eb2a5c10ae
Páldi Ákos <paldi.akos@marktsoft.hu>
2019-10-02 14:45:57 +0200

    Generate a Maven project with Java and Spring Boot

---
+++ src/main/java/hu/elte/marvelcinema/MarvelCinemaApplication.java
@@ -0,0 +1,13 @@
+package hu.elte.marvelcinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelCinemaApplication.class, args);
+	}
+
+}
